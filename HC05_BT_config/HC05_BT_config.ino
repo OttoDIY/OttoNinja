706cf5c8fe6b1c13df052bc872e8fcdd5a29a663
Camilo <cparrapa@gmail.com>
2016-08-26 16:15:04 +0800

    Bluetooth configurators

---
+++ HC05_BT_config/HC05_BT_config.ino
@@ -0,0 +1,32 @@
+/*
+ConfigBluetooth HC-05 from Arduino.
+Change name, password and baud using serial port with AT commands, only configurate one time
+After upload the sketch connect your module, the LED should blinking every 2 seconds.
+If LED is blinking too fast, you need to disconnect and connect again but this time pressing the reset little button of the BT module
+Then you can open your serial (Both NL & CR 9600 baud) and should appear and "OK" message means is ready to recieve AT commands. (AT+NAME=)
+After setup you can pair your module
+*/
+#include <SoftwareSerial.h>
+
+SoftwareSerial BTSerial(10, 11); // RX | TX
+
+void setup()
+{
+  pinMode(9, OUTPUT);  // this pin will pull the HC-05 pin 34 (key pin) HIGH to switch module to AT mode
+  digitalWrite(9, HIGH);
+  Serial.begin(9600);
+  Serial.println("Enter AT commands:");
+  BTSerial.begin(38400);  // HC-05 default speed in AT command more
+}
+
+void loop()
+{
+
+  // Keep reading from HC-05 and send to Arduino Serial Monitor
+  if (BTSerial.available())
+    Serial.write(BTSerial.read());
+
+  // Keep reading from Arduino Serial Monitor and send to HC-05
+  if (Serial.available())
+    BTSerial.write(Serial.read());
+}
---
+++ HC06_BT_config/HC06_BT_config.ino
@@ -0,0 +1,46 @@
+/*
+	http://giltesa.com/?p=11738
+	09/08/2012
+	(CC) BY-NC-SA: giltesa.com
+
+ConfigBluetooth HC-06 from Arduino.
+Change name, password and baud using serial port, only configurate one time
+After upload code you have 10 seconds to connect the module
+BT ---- Arduino
+TX ----> RX
+RX <---- TX
+
+Once the LED off configuration will start and at the end LED will blink
+After this you can pair your module
+*/
+// Options:
+
+	char ssid[10]		= "Zowi";	// Name for Bluetooth.
+	char baudios		 = '8';		   // 1=>1200 baudios, 2=>2400, 3=>4800, 4=>9600 (default), 5=>19200, 6=>38400, 7=>57600, 8=>115200
+	char password[10]	= "1234";		// Password for pairing
+
+void setup()
+{
+	Serial.begin(9600);
+	
+	// Waiting time (10 seconds):
+		pinMode(9,OUTPUT);
+		digitalWrite(9,HIGH);
+		delay(10000);
+		digitalWrite(9,LOW);
+	
+Serial.print("AT"); delay(1000); // Now configuration start
+
+Serial.print("AT+NAME"); Serial.print(ssid); delay(1000);   // Change Name of BT
+
+Serial.print("AT+BAUD"); Serial.print(baudios); delay(1000);    // Change Baud
+
+Serial.print("AT+PIN"); Serial.print(password); delay(1000);	    // Change Password
+}
+
+void loop()
+{
+	// After LED Blinking is finish.
+	digitalWrite(9, !digitalRead(9));
+	delay(500);
+}
