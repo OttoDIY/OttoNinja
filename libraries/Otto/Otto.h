9e386971a8dd6eed082dd427d0425251d9ffd88b
Camilo <cparrapa@gmail.com>
2018-05-11 13:13:14 +0800

    sound pin to 13

--- libraries/Otto/Otto.h
+++ libraries/Otto/Otto.h
@@ -23,7 +23,7 @@
 #define MEDIUM      15
 #define BIG         30
 
-#define PIN_Buzzer  10
+#define PIN_Buzzer  13
 #define PIN_Trigger 8
 #define PIN_Echo    9
 #define PIN_NoiseSensor A6
@@ -120,5 +120,3 @@ class Otto
 };
 
 #endif
-
-
