c962286213ca2a0be059e0f6502125d6088922c6
Camilo <cparrapa@gmail.com>
2018-10-24 10:19:17 +0800

    port 13

--- birthdaysong/birthdaysong.ino
+++ birthdaysong/birthdaysong.ino
@@ -1,46 +1,23 @@
 //ONG LIT YIT 2013-07-20
 //This following code plays happy birthday melody on Arduino
-//Put Piezo Buzzer on GDN and 9 (Positive and negative are reversible)
-//this project requires a Piezo Buzzer and
-
-// an Arduino board and
-
-//jumper wires to connect Buzzer's (+) to ~9 and (-) to GND (any GND)
-
+//Put Piezo Buzzer on GDN and 13 (Positive and negative are reversible)
+//this project requires a Piezo Buzze an Arduino board and jumper wires to connect Buzzer's (+) to ~13 and (-) to GND (any GND)
 //HAVE FUN
 
-//this project requires a Piezo Buzzer and
-
-// an Arduino board and
-
-//jumper wires to connect Buzzer's (+) to ~9 and (-) to GND (any GND)
-
-//HAVE FUN
-
-int speakerPin = 10;
-
+int speakerPin = 13;
 int length = 28; // the number of notes
-
 char notes[] = "GGAGcB GGAGdc GGxecBA yyecdc";
-
 int beats[] = { 2, 2, 8, 8, 8, 16, 1, 2, 2, 8, 8,8, 16, 1, 2,2,8,8,8,8,16, 1,2,2,8,8,8,16 };
-
 int tempo = 150;
 
 void playTone(int tone, int duration) {
 
 for (long i = 0; i < duration * 1000L; i += tone * 2) {
-
    digitalWrite(speakerPin, HIGH);
-
    delayMicroseconds(tone);
-
    digitalWrite(speakerPin, LOW);
-
    delayMicroseconds(tone);
-
 }
-
 }
 
 void playNote(char note, int duration) {
@@ -58,45 +35,28 @@ int tones[] = { 1915, 1700, 1519, 1432, 1275, 1136, 1014,
                  655 , 715 };
 
 int SPEE = 5;
-
 // play the tone corresponding to the note name
 
 for (int i = 0; i < 17; i++) {
-
    if (names[i] == note) {
     int newduration = duration/SPEE;
      playTone(tones[i], newduration);
-
    }
-
 }
-
 }
 
 void setup() {
-
 pinMode(speakerPin, OUTPUT);
-
 }
 
 void loop() {
-
 for (int i = 0; i < length; i++) {
-
    if (notes[i] == ' ') {
-
      delay(beats[i] * tempo); // rest
-
    } else {
-
      playNote(notes[i], beats[i] * tempo);
-
    }
-
    // pause between notes
-
    delay(tempo);
-
 }
-
 }
