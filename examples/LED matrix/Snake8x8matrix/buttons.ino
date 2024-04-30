c2fd0e2bb533f505b18cb12a023ecc3eb51001e1
jaiwiwjwjwisn <168209237+jaiwiwjwjwisn@users.noreply.github.com>
2024-05-01 00:53:47 +0600

    comment

--- examples/LED matrix/Snake8x8matrix/buttons.ino
+++ examples/LED matrix/Snake8x8matrix/buttons.ino
@@ -1,19 +1,28 @@
+// Define an integer pointer for storing button pin numbers
 int* buttonPin;
+
+// Define an integer pointer for storing button states
 int* buttonState;
 
+/**
+ * Initialize button pins and states
+ * @param pinNumbers An array of button pin numbers
+ * @param length The number of buttons
+ */
 void initButtons(int pinNumbers[], int length){
   Serial.println("initButtons");
   // Copy PIN numbers
-  buttonPin = new int[length];
+  buttonPin = new int[length]; // Allocate memory for buttonPin array
   for(int i=0; i<length; i++){
-    buttonPin[i] = pinNumbers[i];
+    buttonPin[i] = pinNumbers[i]; // Copy pinNumbers array elements to buttonPin array
   }
   // Initialize button state
-  buttonState = new int[length];
+  buttonState = new int[length]; // Allocate memory for buttonState array
   for(int i=0; i<length; i++){
-    buttonState[i] = LOW;
+    buttonState[i] = LOW; // Initialize all button states to LOW
   }
   
+  // Print button pin numbers and states for debugging
   for(int i=0; i<length; i++)
   {
     Serial.print(buttonPin[i]);
@@ -22,16 +31,22 @@ void initButtons(int pinNumbers[], int length){
   
 }
 
+/**
+ * Check if a button is clicked
+ * @param index The index of the button to check
+ * @return true if the button is clicked, false otherwise
+ */
 boolean buttonClicked(int index){
   // current state
-  int state = digitalRead(buttonPin[index]);
-  boolean buttonClick = false;
+  int state = digitalRead(buttonPin[index]); // Read the current state of the button
+  boolean buttonClick = false; // Initialize buttonClick to false
   // button has pressed
-  if(buttonState[index] == HIGH){
-    if(state == LOW){
-      buttonClick = true;
+  if(buttonState[index] == HIGH){ // If the button was previously pressed
+    if(state == LOW){ // And it is now released
+      buttonClick = true; // The button is clicked
     }
   }
-  buttonState[index] = state;
-  return buttonClick;
+  buttonState[index] = state; // Update the button state
+  return buttonClick; // Return the buttonClick value
 }
+
