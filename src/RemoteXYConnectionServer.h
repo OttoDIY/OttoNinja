15c9e79a8ccfb1f6e2be6891a10012222d88fcad
jaiwiwjwjwisn <168209237+jaiwiwjwjwisn@users.noreply.github.com>
2024-05-01 00:53:54 +0600

    comment

--- src/RemoteXYConnectionServer.h
+++ src/RemoteXYConnectionServer.h
@@ -1,41 +1,51 @@
 #ifndef RemoteXYConnectionServer_h
 #define RemoteXYConnectionServer_h
 
+// Include necessary header files
 #include "RemoteXYDebugLog.h"
 #include "RemoteXYConnection.h"
 #include "RemoteXYWireStream.h"
 #include "RemoteXYThread.h"
 
-
-
 class CRemoteXYConnectionServer: public CRemoteXYConnectionComm, public CRemoteXYClientAvailableListener { 
   public:
+  // Port number for the server to listen on
   uint16_t port;
+  // Pointer to the data object
   CRemoteXYData * data;
+  // Pointer to the server object
   CRemoteXYServer * server;
+  // Pointer to the clients list
   CRemoteXYClient * clients;
+  // Pointer to the wires list
   CRemoteXYWireStream * wires;
+  // Flag to check if the server is running
   uint8_t serverRunning;
-  
-  
+
+  // Constructor for the CRemoteXYConnectionServer class
   CRemoteXYConnectionServer (CRemoteXYComm * _comm, uint16_t _port = 0) : CRemoteXYConnectionComm (_comm) {
     port = _port;
     clients = NULL;
     server = comm->createServer (port); 
     server->setClientAvailabListener (this);
+
+    // Debug logging for server creation
 #if defined(REMOTEXY__DEBUGLOG)
     if (!server) {
       RemoteXYDebugLog.write ("Server was not created");
     }
 #endif     
+
     serverRunning = 0;
     wires = NULL;  
   }
-  
+
+  // Initialize the data object
   void init (CRemoteXYData * _data) {
     data = _data;
   }
-  
+
+  // Handler function to manage server and client connections
   public:  
   void handler () override {
     if (!server) return;
@@ -44,11 +54,15 @@ class CRemoteXYConnectionServer: public CRemoteXYConnectionComm, public CRemoteX
       else {
         if (server->begin ()) {
           serverRunning=1;
+
+          // Debug logging for server start
 #if defined(REMOTEXY__DEBUGLOG)
           RemoteXYDebugLog.write ("Server opened on port ");
           RemoteXYDebugLog.writeAdd (port);
 #endif                
         }
+
+        // Debug logging for server start failure
 #if defined(REMOTEXY__DEBUGLOG)
         else {
           RemoteXYDebugLog.write ("Server was not started");
@@ -63,7 +77,8 @@ class CRemoteXYConnectionServer: public CRemoteXYConnectionComm, public CRemoteX
       }
     }
   }
-  
+
+  // Client availability handler
   void clientAvailable () override {  
     CRemoteXYClient * client = clients;
     while (client) {
@@ -76,7 +91,8 @@ class CRemoteXYConnectionServer: public CRemoteXYConnectionComm, public CRemoteX
       clients = client;
     }
     if (server->available (client)) {
-      
+
+      // Check if the maximum number of clients is not exceeded
       if (CRemoteXYThread::runningCount (data) < REMOTEXY_MAX_CLIENTS) { 
         CRemoteXYWireStream * wire = wires;
         while (wire) {         
@@ -89,10 +105,14 @@ class CRemoteXYConnectionServer: public CRemoteXYConnectionComm, public CRemoteX
           wires = wire;        
         }
         wire->begin (client);
+
+        // Start the thread for the new client
         CRemoteXYThread::startThread (data, this, wire, 1);        
       }
       else {
         client->stop ();
+
+        // Debug logging for client rejection
 #if defined(REMOTEXY__DEBUGLOG)
         RemoteXYDebugLog.write ("Client reject");
 #endif  
@@ -100,7 +120,8 @@ class CRemoteXYConnectionServer: public CRemoteXYConnectionComm, public CRemoteX
     }
       
   }
-  
+
+  // Wire handler function
   void handleWire (CRemoteXYWire * wire) override {
     CRemoteXYClient * client = ((CRemoteXYWireStream*)wire)->getClient ();
     if (client) {
@@ -109,6 +130,7 @@ class CRemoteXYConnectionServer: public CRemoteXYConnectionComm, public CRemoteX
     }
   }
 
+  // Stop the thread listener function
   void stopThreadListener (CRemoteXYWire * wire) override {
     CRemoteXYClient * client = ((CRemoteXYWireStream*)wire)->getClient ();
     if (client) {
@@ -116,9 +138,8 @@ class CRemoteXYConnectionServer: public CRemoteXYConnectionComm, public CRemoteX
       wire->stop ();
     }
   }
-  
 
-  
+
 };
 
 
