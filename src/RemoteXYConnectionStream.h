f0f096680d2f6accdd5ea673c22b8b68bfd3e97b
jaiwiwjwjwisn <168209237+jaiwiwjwjwisn@users.noreply.github.com>
2024-05-01 00:57:59 +0600

    improve

--- src/RemoteXYConnectionStream.h
+++ src/RemoteXYConnectionStream.h
@@ -1,37 +1,35 @@
-#ifndef RemoteXYConnectionStream_h
-#define RemoteXYConnectionStream_h
+#ifndef REMOTEXY_CONNECTION_STREAM_H
+#define REMOTEXY_CONNECTION_STREAM_H
 
 #include "RemoteXYDebugLog.h"
 #include "RemoteXYConnection.h"
 #include "RemoteXYThread.h"
+#include "RemoteXYStream.h"
 
 // Define the CRemoteXYConnectionStream class which inherits from CRemoteXYConnection
-class CRemoteXYConnectionStream: public CRemoteXYConnection {
-  // Stream object of type CRemoteXYStream
-  CRemoteXYStream * stream;
+class CRemoteXYConnectionStream : public CRemoteXYConnection {
+  // Pointer to the stream object
+  CRemoteXYStream *stream;
 
   public: 
   // Constructor with a CRemoteXYStream object as parameter
-  CRemoteXYConnectionStream (CRemoteXYStream * _stream) {
-    stream = _stream;
-  }    
-  
+  CRemoteXYConnectionStream(CRemoteXYStream *stream) : stream(stream) {}
+
   // Override the init method from CRemoteXYConnection
-  void init (CRemoteXYData * _data) override {
-    // Create a new CRemoteXYWireStream object with _data as parameter
-    CRemoteXYWireStream * wire = new CRemoteXYWireStream (_data);
+  void init(CRemoteXYData *data) override {
+    // Create a new CRemoteXYWireStream object with data as parameter
+    CRemoteXYWireStream *wireStream = new CRemoteXYWireStream(data);
     // Initialize the wire object with the stream object
-    wire->begin (stream);  
-    // Start a new thread with _data, this (CRemoteXYConnectionStream object), wire, and 0 as parameters
-    CRemoteXYThread::startThread (_data, this, wire, 0);
+    wireStream->begin(stream);
+    // Start a new thread with data, this (CRemoteXYConnectionStream object), wireStream, and 0 as parameters
+    CRemoteXYThread::startThread(data, this, wireStream, 0);
   };
-  
+
   // Override the handleWire method from CRemoteXYConnection
-  void handleWire (CRemoteXYWire * wire) override {
+  void handleWire(CRemoteXYWire *wire) override {
     // Call the handler method of the wire object
-    wire->handler ();
-  }; 
-  
+    wire->handler();
+  };
 };
 
-#endif // CRemoteXYConnectionStream
+#endif // REMOTEXY_CONNECTION_STREAM_H
