c36d8c332c145b56215204ce5eedfe24f8333b74
jaiwiwjwjwisn <168209237+jaiwiwjwjwisn@users.noreply.github.com>
2024-05-01 00:57:58 +0600

    improve

--- src/RemoteXYStream.h
+++ src/RemoteXYStream.h
@@ -1,53 +1,35 @@
 #ifndef RemoteXYStream_h
-#define RemoteXYStream_h   // Include guard to prevent multiple inclusions
+#define RemoteXYStream_h
 
-#include <inttypes.h>  // Include standard integer type definitions
-#include "RemoteXYDebugLog.h"  // Include debug logging utility header
+#include <inttypes.h>
+#include "RemoteXYDebugLog.h"
 
-#define UNUSED(x) (void)(x)  // Macro to suppress warnings for unused variables
+#define UNUSED(x) (void)(x)
 
-// Forward declaration of the listener interface
 class CRemoteXYReadByteListener;
 
-// The main RemoteXYStream class
 class CRemoteXYStream {
   private:
-    CRemoteXYReadByteListener * readByteListener;  // Pointer to the read byte listener
+    CRemoteXYReadByteListener* readByteListener;
 
   public:
-    // Constructor
-    CRemoteXYStream () {
-      readByteListener = NULL;
-
+    CRemoteXYStream() : readByteListener(nullptr) {
 #if defined(REMOTEXY__DEBUGLOG)
-      // Initialize the debug logging utility if debug logging is enabled
-      RemoteXYDebugLog.init ();
+      RemoteXYDebugLog.init();
 #endif
     }
 
-  public:
-    // Set the read byte listener
-    void setReadByteListener (CRemoteXYReadByteListener * listener) {
+    void setReadByteListener(CRemoteXYReadByteListener* listener) {
       readByteListener = listener;
     }
 
-  public:
-    // Notify the read byte listener with a received byte
-    void notifyReadByteListener (uint8_t byte) {
-      if (readByteListener) readByteListener->readByte (byte);
+    void notifyReadByteListener(uint8_t byte) {
+      if (readByteListener) readByteListener->readByte(byte);
     }
 
-  public:     
-    // Placeholder functions for the abstract class
-    // These functions will be overridden in derived classes
-    virtual void handler () {};
-    virtual void startWrite (uint16_t size) {UNUSED (size);};
-    virtual void write (uint8_t byte) {UNUSED (byte);};
+    virtual void handler() {}
+    virtual void startWrite(uint16_t size) {UNUSED(size);}
+    virtual void write(uint8_t byte) {UNUSED(byte);}
 };
 
-
-
-
-
 #endif //RemoteXYStream_h
-
