eeb8f573acd4dded8e6d6c1292ac686b51abed93
jaiwiwjwjwisn <168209237+jaiwiwjwjwisn@users.noreply.github.com>
2024-05-01 00:53:50 +0600

    comment

--- src/RemoteXYStream_BluetoothSerial.h
+++ src/RemoteXYStream_BluetoothSerial.h
@@ -1,34 +1,33 @@
-#ifndef RemoteXYStream_BluetoothSerial_h
-#define RemoteXYStream_BluetoothSerial_h
+#ifndef RemoteXYStream_BluetoothSerial_h // If the RemoteXYStream_BluetoothSerial header file hasn't been included yet
+#define RemoteXYStream_BluetoothSerial_h // Define the RemoteXYStream_BluetoothSerial header file
 
-#if defined (BluetoothSerial_h) || defined (_BLUETOOTH_SERIAL_H_)
+#if defined (BluetoothSerial_h) || defined (_BLUETOOTH_SERIAL_H_) // If the BluetoothSerial header file has been included
 
-#include "RemoteXYComm.h"
+#include "RemoteXYComm.h" // Include the RemoteXYComm library
 
+class CRemoteXYStream_BluetoothSerial : public CRemoteXYStream { // Create a new class named CRemoteXYStream_BluetoothSerial that inherits from CRemoteXYStream
 
-class CRemoteXYStream_BluetoothSerial : public CRemoteXYStream {
-  
   private:
-  BluetoothSerial * serial;
-  
-  public:
-  CRemoteXYStream_BluetoothSerial (const char * _bleDeviceName) : CRemoteXYStream () {
-    serial = new  BluetoothSerial ();
-    serial->begin (_bleDeviceName);
+  BluetoothSerial * serial; // Declare a pointer to a BluetoothSerial object
 
-#if defined(REMOTEXY__DEBUGLOG)
-    RemoteXYDebugLog.write("Init bluetooth serial");
+  public:
+  CRemoteXYStream_BluetoothSerial (const char * _bleDeviceName) : CRemoteXYStream () { // Constructor for the CRemoteXYStream_BluetoothSerial class
+    serial = new  BluetoothSerial (); // Create a new BluetoothSerial object
+    serial->begin (_bleDeviceName); // Begin the BluetoothSerial object with the given device name
+
+#if defined(REMOTEXY__DEBUGLOG) // If the REMOTEXY__DEBUGLOG macro is defined
+    RemoteXYDebugLog.write("Init bluetooth serial"); // Write "Init bluetooth serial" to the debug log
 #endif
   }              
   
   
-  void handler () override {   
-    while (serial->available ()) notifyReadByteListener (serial->read ());
+  void handler () override { // Override the handler function from the CRemoteXYStream class
+    while (serial->available ()) notifyReadByteListener (serial->read ()); // While there are bytes available to read from the BluetoothSerial object, read each byte and notify the listener
   }
 
 
-  void write (uint8_t byte) override {
-    serial->write (byte);
+  void write (uint8_t byte) override { // Override the write function from the CRemoteXYStream class
+    serial->write (byte); // Write the given byte to the BluetoothSerial object
   }
 
 
