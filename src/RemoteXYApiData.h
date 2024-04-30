24b4c4f47828c8edc0b641a1706d972dae177460
jaiwiwjwjwisn <168209237+jaiwiwjwjwisn@users.noreply.github.com>
2024-05-01 00:55:57 +0600

    comment

--- src/RemoteXYApiData.h
+++ src/RemoteXYApiData.h
@@ -1,85 +1,56 @@
-#ifndef RemoteXYApiData_h
-#define RemoteXYApiData_h  
-
-#include <inttypes.h> 
-
-
-#define REMOTEXY_PASSWORD_LENGTH_MAX 26
-
-class CRemoteXYThread;
-class CRemoteXYComm;
-class CRemoteXYConnectionComm;
-
+// CRemoteXYData class
+// This class handles data related to a remote connection.
+// It includes variables for configuration data, input and output data,
+// a connection flag, and various other settings.
 
 class CRemoteXYData {
-
   public:
+  // confVersion: The version of the configuration data.
   uint8_t confVersion;
+
+  // conf: A pointer to the configuration data.
   uint8_t *conf;
+
+  // var: A pointer to the variable data.
   uint8_t *var;
+
+  // accessPassword: A pointer to the access password for the remote connection.
   uint8_t *accessPassword;
+
+  // outputLength: The length of the output data.
   uint16_t outputLength;
+
+  // inputLength: The length of the input data.
   uint16_t inputLength;
+
+  // confLength: The length of the configuration data.
   uint16_t confLength;
+
+  // connect_flag: A flag indicating whether the device is currently connected to the remote system.
   uint8_t *connect_flag;
 
+  // receiveBufferSize: The size of the receive buffer.
   uint16_t receiveBufferSize;  
-  
+
+  // threads: A pointer to an instance of the CRemoteXYThread class.
   CRemoteXYThread * threads;  
+
+  // comms: A pointer to an instance of the CRemoteXYComm class.
   CRemoteXYComm * comms;    
+
+  // connections: A pointer to an instance of the CRemoteXYConnectionComm class.
   CRemoteXYConnectionComm * connections;    
 
 
   public:
-  void init (const void * _conf, void * _var, const char * _accessPassword) {
-  
-    uint8_t i;
-    uint8_t* p = (uint8_t*)_conf;
-    uint8_t b = getConfByte (p++);
+  // init: Initializes the CRemoteXYData object with configuration, variable, and access password data.
+  // _conf: A pointer to the configuration data.
+  // _var: A pointer to the variable data.
+  // _accessPassword: A pointer to the access password for the remote connection.
+  void init (const void * _conf, void * _var, const char * _accessPassword);
 
-    if (b==0xff) {
-      inputLength = getConfByte (p++);
-      inputLength |= getConfByte (p++)<<8;
-      outputLength = getConfByte (p++);
-      outputLength |= getConfByte (p++)<<8;
-    }
-    else {
-      inputLength = b;
-      outputLength = getConfByte (p++);    
-    }
-    confLength = getConfByte (p++);
-    confLength |= getConfByte (p++)<<8;
-    conf = p;
-    confVersion = getConfByte (p);
-    var = (uint8_t*)_var;
-    uint16_t varLength = outputLength + inputLength;
-    connect_flag = var + varLength;
-    *connect_flag = 0;   
-        
-    accessPassword = (uint8_t*)_accessPassword;          
-    
-    p = var;
-    i = varLength;
-    while (i--) *p++=0;    
-    
-    receiveBufferSize = inputLength;
-    if ((*accessPassword != 0) && (receiveBufferSize < REMOTEXY_PASSWORD_LENGTH_MAX)) receiveBufferSize = REMOTEXY_PASSWORD_LENGTH_MAX;
-    receiveBufferSize +=6;   
-    
-    comms = NULL;
-    connections = NULL;    
-    threads = NULL;
-  }
-  
-  public:
-  inline uint8_t getConfByte (uint8_t* p) {
-    return pgm_read_byte_near (p);                                     
-  }
-  
-  
+  // getConfByte: Returns a byte of configuration data at the specified address.
+  // p: A pointer to the address of the configuration data.
+  inline uint8_t getConfByte (uint8_t* p);
+
 };
-
-
-
-
-#endif //RemoteXYData_h
