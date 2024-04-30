2cba4bcebe55fc3f2bb716ee4267fb3c053eef43
jaiwiwjwjwisn <168209237+jaiwiwjwjwisn@users.noreply.github.com>
2024-05-01 00:58:19 +0600

    improve

--- src/RemoteXYStream_SoftSerial.h
+++ src/RemoteXYStream_SoftSerial.h
@@ -1,18 +1,15 @@
 #ifndef RemoteXYStream_SoftSerial_h
 #define RemoteXYStream_SoftSerial_h
 
-
-// if not defined SoftwareSerial.h
 #if defined(REMOTEXY_MODE__SOFTSERIAL) || defined(REMOTEXY_MODE__ESP8266_SOFTSERIAL_POINT) || defined(REMOTEXY_MODE__SOFTSERIAL_ESP8266) || defined(REMOTEXY_MODE__SOFTSERIAL_ESP8266_CLOUD)
   #if !defined (SoftwareSerial_h) && !defined(SoftSerial_h)
-    #include "SoftwareSerial.h"
-  #endif  
+    #include <SoftwareSerial.h> // Include SoftwareSerial library
+  #endif
 #endif
 
 #if defined(SoftwareSerial_h) || defined(SoftSerial_h)
 
 #include "RemoteXYComm.h"
-
 
 class CRemoteXYStream_SoftSerial : public CRemoteXYStream {
   
@@ -20,42 +17,41 @@ class CRemoteXYStream_SoftSerial : public CRemoteXYStream {
 
   private:
   SoftwareSerial * serial;
-  
+
   public:
   CRemoteXYStream_SoftSerial (uint8_t _serialRx, uint8_t _serialTx, long _serialSpeed) : CRemoteXYStream () { 
     serial = new SoftwareSerial (_serialRx, _serialTx);
     serial->begin (_serialSpeed);
 #if defined(REMOTEXY__DEBUGLOG)
-    RemoteXYDebugLog.write("Init software serial ");
-    RemoteXYDebugLog.writeAdd(_serialSpeed);
-    RemoteXYDebugLog.writeAdd(" baud");
-    RemoteXYDebugLog.write("pin RX=");
-    RemoteXYDebugLog.writeAdd(_serialRx);
-    RemoteXYDebugLog.writeAdd("; pin TX=");
-    RemoteXYDebugLog.writeAdd(_serialTx);
+    RemoteXYDebugLog.print("Init software serial ");
+    RemoteXYDebugLog.print(_serialSpeed);
+    RemoteXYDebugLog.println(" baud");
+    RemoteXYDebugLog.print("pin RX=");
+    RemoteXYDebugLog.print(_serialRx);
+    RemoteXYDebugLog.print("; pin TX=");
+    RemoteXYDebugLog.println(_serialTx);
 #endif
-  }              
-  
+  }
+
 #elif defined(SoftSerial_h)
 
   private:
   SoftSerial * serial;
-  
+
   public:
   CRemoteXYStream_SoftSerial (uint8_t _serialRx, uint8_t _serialTx, long _serialSpeed) : CRemoteXYStream () { 
     serial = new SoftSerial (_serialRx, _serialTx);
     serial->begin (_serialSpeed);
 #if defined(REMOTEXY__DEBUGLOG)
-    RemoteXYDebugLog.write("Init soft serial ");
-    RemoteXYDebugLog.writeAdd(_serialSpeed);
-    RemoteXYDebugLog.writeAdd(" baud");
-    RemoteXYDebugLog.write("pin RX=");
-    RemoteXYDebugLog.writeAdd(_serialRx);
-    RemoteXYDebugLog.writeAdd("; pin TX=");
-    RemoteXYDebugLog.writeAdd(_serialTx);
+    RemoteXYDebugLog.print("Init soft serial ");
+    RemoteXYDebugLog.print(_serialSpeed);
+    RemoteXYDebugLog.println(" baud");
+    RemoteXYDebugLog.print("pin RX=");
+    RemoteXYDebugLog.print(_serialRx);
+    RemoteXYDebugLog.print("; pin TX=");
+    RemoteXYDebugLog.println(_serialTx);
 #endif
-  }              
-
+  }
 
 #endif  
   
@@ -69,7 +65,6 @@ class CRemoteXYStream_SoftSerial : public CRemoteXYStream {
 
   
 };
-
 
 #endif  // SoftwareSerial_h  SoftSerial_h
 
