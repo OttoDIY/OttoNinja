7f17634ced4e37c9f95ad2fac481f789ac0ecd3d
jaiwiwjwjwisn <168209237+jaiwiwjwjwisn@users.noreply.github.com>
2024-05-01 00:58:14 +0600

    improve

--- src/RemoteXYStream.h
+++ src/RemoteXYStream.h
@@ -4,32 +4,54 @@
 #include <inttypes.h>
 #include "RemoteXYDebugLog.h"
 
-#define UNUSED(x) (void)(x)
-
-class CRemoteXYReadByteListener;
-
+/// The CRemoteXYStream class provides a base class for implementing
+/// stream classes that can be used for communication between a
+/// RemoteXY server and client.
 class CRemoteXYStream {
   private:
+    /// The listener object that will be notified when a byte is read.
     CRemoteXYReadByteListener* readByteListener;
 
   public:
+    /// Constructs a new CRemoteXYStream object.
     CRemoteXYStream() : readByteListener(nullptr) {
 #if defined(REMOTEXY__DEBUGLOG)
       RemoteXYDebugLog.init();
 #endif
     }
 
+    /// Destructor for the CRemoteXYStream class.
+    virtual ~CRemoteXYStream() {}
+
+    /// Sets the read byte listener object.
+    /// @param listener The listener object to set.
     void setReadByteListener(CRemoteXYReadByteListener* listener) {
       readByteListener = listener;
     }
 
+    /// Notifies the read byte listener object that a byte has been read.
+    /// @param byte The byte that was read.
     void notifyReadByteListener(uint8_t byte) {
       if (readByteListener) readByteListener->readByte(byte);
     }
 
+    /// The handler method should be implemented by derived classes to
+    /// handle incoming data.
     virtual void handler() {}
-    virtual void startWrite(uint16_t size) {UNUSED(size);}
-    virtual void write(uint8_t byte) {UNUSED(byte);}
+
+    /// Starts writing data to the stream.
+    /// @param size The size of the data to be written.
+    virtual void startWrite(uint16_t size) {
+      // Intentionally left blank.
+      (void)size;
+    }
+
+    /// Writes a byte to the stream.
+    /// @param byte The byte to write.
+    virtual void write(uint8_t byte) {
+      // Intentionally left blank.
+      (void)byte;
+    }
 };
 
 #endif //RemoteXYStream_h
