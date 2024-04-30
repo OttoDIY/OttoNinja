b89a1bf2403d63ce61e5915719c0294904b773cb
jaiwiwjwjwisn <168209237+jaiwiwjwjwisn@users.noreply.github.com>
2024-05-01 00:57:58 +0600

    improve

--- src/RemoteXYStream_BLEPeripheral.h
+++ src/RemoteXYStream_BLEPeripheral.h
@@ -1,9 +1,9 @@
 // Include guard to prevent multiple inclusions of this header file
-#ifndef RemoteXYStream_BLEPeripheral_h
-#define RemoteXYStream_BLEPeripheral_h
+#ifndef REMOTEXY_STREAM_BLE_PERIPHERAL_H
+#define REMOTEXY_STREAM_BLE_PERIPHERAL_H
 
 // If the _BLE_PERIPHERAL_H_ macro is defined, include the RemoteXYStream.h header
-#if defined(_BLE_PERIPHERAL_H_) 
+#if defined(_BLE_PERIPHERAL_H_)
 
 //------------------------------------------------------------------------------
 // Constants and macros for buffer sizes, UUIDs, and timeouts
@@ -12,10 +12,19 @@
 // ... (Constants and macros for buffer sizes, UUIDs, and timeouts)
 
 //------------------------------------------------------------------------------
-// CRemoteXYStream_BLEPeripheral class definition
+// CRemoteXYStreamBLEPeripheral class definition
 //------------------------------------------------------------------------------
-class CRemoteXYStream_BLEPeripheral : public CRemoteXYStream, public BLEPeripheral {
+class CRemoteXYStreamBLEPeripheral : public CRemoteXYStream, public BLEPeripheral {
   private:
     //--------------------------------------------------------------------------
     // Member variables for BLE service, characteristics, and descriptors
-    //------------------------------------------------
+    //--------------------------------------------------------------------------
+
+    // ...
+
+    // Other private member variables, functions, and class definitions
+};
+
+#endif // defined(_BLE_PERIPHERAL_H_)
+
+#endif // REMOTEXY_STREAM_BLE_PERIPHERAL_H
