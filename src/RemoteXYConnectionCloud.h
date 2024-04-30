59ee8da8f66df3f85dbd151f8d4bc64a59309762
jaiwiwjwjwisn <168209237+jaiwiwjwjwisn@users.noreply.github.com>
2024-05-01 00:56:02 +0600

    comment

--- src/RemoteXYConnectionCloud.h
+++ src/RemoteXYConnectionCloud.h
@@ -1,101 +1,102 @@
 #ifndef RemoteXYConnectionCloud_h
 #define RemoteXYConnectionCloud_h
 
+// Include necessary header files
 #include "RemoteXYDebugLog.h"
 #include "RemoteXYConnection.h"
 #include "RemoteXYThread.h"
 
+// Define the timeout value for retrying cloud connection
 #define REMOTEXY_CLOUDCLIENT_RETRY_TIMEOUT 20000
 
-class CRemoteXYConnectionCloud: public CRemoteXYConnectionComm, CRemoteXYCloudClientAvailableListener { 
+// Define the class for cloud connection
+class CRemoteXYConnectionCloud: public CRemoteXYConnectionComm, CRemoteXYCloudClientAvailableListener {
   public:
-  uint16_t port;
-  CRemoteXYCloudServer * cloudServer;
-  const char * cloudHost; 
-  const char * cloudToken;
-  CRemoteXYData * data;
-  CRemoteXYClient * client;
-  uint32_t timeOut;
-    
+    // Public variables
+    uint16_t port;
+    CRemoteXYCloudServer * cloudServer;
+    const char * cloudHost; 
+    const char * cloudToken;
+    CRemoteXYData * data;
+    CRemoteXYClient * client;
+    uint32_t timeOut;
+
   public:
-  CRemoteXYConnectionCloud (CRemoteXYComm * _comm, const char * _cloudHost, uint16_t _port, const char * _cloudToken) : CRemoteXYConnectionComm (_comm) {
-    port = _port;
-    cloudHost = _cloudHost;
-    cloudToken = _cloudToken;
-  }
-  
+    // Constructor for the class
+    CRemoteXYConnectionCloud (CRemoteXYComm * _comm, const char * _cloudHost, uint16_t _port, const char * _cloudToken) : CRemoteXYConnectionComm (_comm) {
+      port = _port;
+      cloudHost = _cloudHost;
+      cloudToken = _cloudToken;
+    }
+
   public:
-  void init (CRemoteXYData * _data) override {
-    data = _data;
-    cloudServer = new CRemoteXYCloudServer (data, cloudToken, this);
-    client = comm->newClient ();
-    timeOut = -REMOTEXY_CLOUDCLIENT_RETRY_TIMEOUT;
-  }
-  
-  void handler () override {
-    
-    if (comm->configured ()) {
-      if (cloudServer->running ()) {
-        cloudServer->handler();     
-        timeOut = millis();      
-      }
-      else { // not serverRunning
-        if (millis() - timeOut > REMOTEXY_CLOUDCLIENT_RETRY_TIMEOUT) {
-#if defined(REMOTEXY__DEBUGLOG)
-          RemoteXYDebugLog.write ("Connecting to cloud: ");
-          RemoteXYDebugLog.writeAdd (cloudHost);
-          RemoteXYDebugLog.writeAdd (" ");
-          RemoteXYDebugLog.writeAdd (port);
-          RemoteXYDebugLog.writeAdd (" ..");
-#endif 
-          if (client->connect (cloudHost, port)) {
-#if defined(REMOTEXY__DEBUGLOG)
-            RemoteXYDebugLog.write ("Cloud server connected");
-#endif 
-            cloudServer->begin (client);            
-          }        
-#if defined(REMOTEXY__DEBUGLOG)
-          else {
-            RemoteXYDebugLog.write ("Cloud server not available");
+    // Initialize the connection
+    void init (CRemoteXYData * _data) override {
+      data = _data;
+      cloudServer = new CRemoteXYCloudServer (data, cloudToken, this);
+      client = comm->newClient ();
+      timeOut = -REMOTEXY_CLOUDCLIENT_RETRY_TIMEOUT;
+    }
+
+    // Handle the connection
+    void handler () override {
+      // Check if the communication is configured
+      if (comm->configured ()) {
+        // If the cloud server is running
+        if (cloudServer->running ()) {
+          cloudServer->handler();     
+          timeOut = millis();      
+        }
+        // If the cloud server is not running
+        else { // not serverRunning
+          // If the timeout has been reached
+          if (millis() - timeOut > REMOTEXY_CLOUDCLIENT_RETRY_TIMEOUT) {
+            // Try to connect to the cloud server
+            if (client->connect (cloudHost, port)) {
+              cloudServer->begin (client);            
+              timeOut = millis();   
+            }
           }
-#endif     
-          timeOut = millis();   
         }
       }
-    }
-    else cloudServer->stop ();
-    if (!cloudServer->running ()) {
-      if (client->connected ()) {
-        client->stop ();
+      // If the communication is not configured
+      else cloudServer->stop ();
+      // If the cloud server is not running
+      if (!cloudServer->running ()) {
+        // If the client is connected
+        if (client->connected ()) {
+          client->stop ();
+        }
+      }
+    }  
+
+    // Handle the cloud wire when it is available
+    void clientAvailable (CRemoteXYWireCloud * cloudWire) override {
+      // If the number of running clients is less than the maximum limit
+      if (CRemoteXYThread::runningCount (data) < REMOTEXY_MAX_CLIENTS) { 
+        CRemoteXYThread::startThread (data, this, cloudWire, 1);  
+      }
+      // If the number of running clients is greater than or equal to the maximum limit
+      else {
+        cloudWire->stop ();
+#if defined(REMOTEXY__DEBUGLOG)
+        RemoteXYDebugLog.write ("Client reject");
+#endif           
       }
     }
-  }  
-  
-  void clientAvailable (CRemoteXYWireCloud * cloudWire) override {
-    if (CRemoteXYThread::runningCount (data) < REMOTEXY_MAX_CLIENTS) { 
-      CRemoteXYThread::startThread (data, this, cloudWire, 1);  
-    }
-    else {
-      cloudWire->stop ();
-#if defined(REMOTEXY__DEBUGLOG)
-      RemoteXYDebugLog.write ("Client reject");
-#endif           
-    }
-  
-  }
 
-  
-  void handleWire (CRemoteXYWire * wire) override {  
-    if (cloudServer->running () && comm->configured ()) wire->handler (); 
-    else stopThreadListener (wire);
-  }
-  
-  void stopThreadListener (CRemoteXYWire * wire) override {
-    wire->stop ();
-  }
-  
+    // Handle the wire
+    void handleWire (CRemoteXYWire * wire) override {  
+      // If the cloud server is running and the communication is configured
+      if (cloudServer->running () && comm->configured ()) wire->handler (); 
+      // If the cloud server is not running
+      else stopThreadListener (wire);
+    }
 
-  
+    // Stop the thread listener
+    void stopThreadListener (CRemoteXYWire * wire) override {
+      wire->stop ();
+    }
 };
 
 
