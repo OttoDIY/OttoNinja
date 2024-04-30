6252f30aa661bb44f8b711524949c3e2531a1f6d
jaiwiwjwjwisn <168209237+jaiwiwjwjwisn@users.noreply.github.com>
2024-05-01 00:53:52 +0600

    comment

--- src/RemoteXYApi.h
+++ src/RemoteXYApi.h
@@ -1,10 +1,9 @@
-#ifndef RemoteXYApi_h
-#define RemoteXYApi_h
-                        
-#include <inttypes.h> 
+// Include necessary header files
+#include <inttypes.h>
 #include <stdlib.h>
 #include <Arduino.h>
 
+// Include header files for related classes and libraries
 #include "RemoteXYDebugLog.h"
 #include "RemoteXYApiData.h"
 #include "RemoteXYConnection.h"
@@ -20,119 +19,98 @@
 
 #include "RemoteXYCloudServer.h"
 
-
-
+// Define the class CRemoteXY
 class CRemoteXY {
   public:
-  CRemoteXYData data;  
+    // Property: data - an instance of the CRemoteXYData class
+    CRemoteXYData data;
 
+    // Constructor 1: Initialize the CRemoteXY object with a configuration, variables, and an access password
+    CRemoteXY(const void *_conf, void *_var, const char *_accessPassword) {
+      data.init(_conf, _var, _accessPassword);
 
-  public:
-  CRemoteXY (const void * _conf, void * _var, const char * _accessPassword) {                     
-    data.init (_conf, _var, _accessPassword);
-    
 #if defined(REMOTEXY__DEBUGLOG)
-    RemoteXYDebugLog.init ();
-    RemoteXYDebugLog.write("RemoteXY started");
+      RemoteXYDebugLog.init();
+      RemoteXYDebugLog.write("RemoteXY started");
 #endif 
-  }
-   
-  public:
-  CRemoteXY (const void * _conf, void * _var, const char * _accessPassword, CRemoteXYConnectionComm * _conn) {                     
-    data.init (_conf, _var, _accessPassword);
-    addConnection (_conn);
-  
+    }
+
+    // Constructor 2: Initialize the CRemoteXY object with a configuration, variables, an access password, and a connection
+    CRemoteXY(const void *_conf, void *_var, const char *_accessPassword, CRemoteXYConnectionComm *_conn) {
+      data.init(_conf, _var, _accessPassword);
+      addConnection(_conn);
+
 #if defined(REMOTEXY__DEBUGLOG)
-    RemoteXYDebugLog.init ();
-    RemoteXYDebugLog.write("RemoteXY started");
+      RemoteXYDebugLog.init();
+      RemoteXYDebugLog.write("RemoteXY started");
 #endif 
-  }
-  
-  public:
-  CRemoteXY (const void * _conf, void * _var, const char * _accessPassword, CRemoteXYStream * _comm) {                     
-    data.init (_conf, _var, _accessPassword);
-    addConnection (_comm);
-  
+    }
+
+    // Constructor 3: Initialize the CRemoteXY object with a configuration, variables, an access password, and a communication stream
+    CRemoteXY(const void *_conf, void *_var, const char *_accessPassword, CRemoteXYStream *_comm) {
+      data.init(_conf, _var, _accessPassword);
+      addConnection(_comm);
+
 #if defined(REMOTEXY__DEBUGLOG)
-    RemoteXYDebugLog.init ();
-    RemoteXYDebugLog.write("RemoteXY started");
+      RemoteXYDebugLog.init();
+      RemoteXYDebugLog.write("RemoteXY started");
 #endif 
-  }
-  
-
-  
-  public:  
-  void addConnection (CRemoteXYConnectionComm * conn) {   
-    CRemoteXYComm * p = data.comms;
-    while (p) {
-      if (p == conn->comm) break;
-      p = p->next;
     }
-    if (!p) {
-      conn->comm->next = data.comms;
-      data.comms = conn->comm;
-    }
-    conn->next = data.connections;
-    data.connections = conn; 
-    conn->init (&data);    
-  } 
-          
-  public:  
-  void addConnection (CRemoteXYStream * comm) {  
-    CRemoteXYConnection * conn = new CRemoteXYConnectionStream (comm);
-    conn->init (&data);           
-  } 
-  
-  
-  public:
-  void handler () {
-    uint8_t connect_flag = 0;
-    
-    // threads handler
 
-    CRemoteXYThread * pt = data.threads;
-    while (pt) {   
-      pt->handler ();     
-      connect_flag += pt->connect_flag;
-      pt = pt->next;
+    // Method: addConnection - Add a connection to the CRemoteXY object
+    void addConnection(CRemoteXYConnectionComm *conn) {
+      CRemoteXYComm *p = data.comms;
+      while (p) {
+        if (p == conn->comm) break;
+        p = p->next;
+      }
+      if (!p) {
+        conn->comm->next = data.comms;
+        data.comms = conn->comm;
+      }
+      conn->next = data.connections;
+      data.connections = conn; 
+      conn->init(&data);    
     }
-    *data.connect_flag = connect_flag;
 
-    // communications handler    
-    
-    CRemoteXYComm * comm = data.comms; 
-    while (comm) {
-      comm->handler (); 
-      comm = comm->next;
+    // Method: addConnection - Add a communication stream to the CRemoteXY object
+    void addConnection(CRemoteXYStream *comm) {
+      CRemoteXYConnection *conn = new CRemoteXYConnectionStream(comm);
+      conn->init(&data);           
     } 
-    
-    // connections handler    
 
-    CRemoteXYConnectionComm * connection = data.connections; 
-    while (connection) {
-      connection->handler (); 
-      connection = connection->next;
-    }    
-      
+    // Method: handler - Handle communication and connections
+    void handler() {
+      uint8_t connect_flag = 0;
 
-  }   
+      // Handle threads
+      CRemoteXYThread *pt = data.threads;
+      while (pt) {   
+        pt->handler();     
+        connect_flag += pt->connect_flag;
+        pt = pt->next;
+      }
+      *data.connect_flag = connect_flag;
 
-  
+      // Handle communications
+      CRemoteXYComm *comm = data.comms; 
+      while (comm) {
+        comm->handler(); 
+        comm = comm->next;
+      } 
 
-///////////////////////////////////////////////////////////////////////////////
-// API - PUBLIC DOCUMENTED FUNCTIONS
+      // Handle connections
+      CRemoteXYConnectionComm *connection = data.connections; 
+      while (connection) {
+        connection->handler(); 
+        connection = connection->next;
+      }    
+    }
 
+    // Method: isConnected - Check if a connection is established
+    uint8_t isConnected() {
+      return *data.connect_flag;
+    }
 
-  public:
-  uint8_t isConnected () {
-    return *data.connect_flag;
-  }
-  
 };
-
-
-
-
-   
-#endif //RemoteXYApi_h
 
