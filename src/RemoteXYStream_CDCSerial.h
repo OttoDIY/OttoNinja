4b915e4bf358ec431e66036a08f02bc35880f2eb
jaiwiwjwjwisn <168209237+jaiwiwjwjwisn@users.noreply.github.com>
2024-05-01 22:33:52 +0600

    comment

--- src/RemoteXYStream_CDCSerial.h
+++ src/RemoteXYStream_CDCSerial.h
@@ -1,24 +1,37 @@
 #ifndef RemoteXYStream_CDCSerial_h
 #define RemoteXYStream_CDCSerial_h
 
+// Guard to prevent multiple inclusions of this header file
+
 #if defined(USBCON)
+
+// Include necessary header files only if USBCON is defined
 
 #include <Stream.h>
 #include "RemoteXYStream.h"
 
 class CRemoteXYStream_CDCSerial : public CRemoteXYStream {
   
+// Class definition for CRemoteXYStream_CDCSerial that inherits from CRemoteXYStream
+
   private:
+  // Private member variables
   Stream * serial;
   long serialSpeed;
 
   public:
+  // Public member functions and constructors
+
   CRemoteXYStream_CDCSerial (Stream * _serial, long _serialSpeed) : CRemoteXYStream () {
+    // Constructor with initializer list
+    // Sets the serial object and serial speed
+
     serial = _serial;
     serialSpeed = _serialSpeed;
     serial->begin(serialSpeed);
 
 #if defined(REMOTEXY__DEBUGLOG)
+    // Debug logging if REMOTEXY__DEBUGLOG is defined
     RemoteXYDebugLog.print("Init CDC serial ");
     RemoteXYDebugLog.print(serialSpeed);
     RemoteXYDebugLog.println(" baud");
@@ -26,10 +39,16 @@ class CRemoteXYStream_CDCSerial : public CRemoteXYStream {
   }
 
   void handler() override {
+    // Overrides the handler function from CRemoteXYStream
+    // Handles incoming serial data and notifies the read byte listener
+
     while (serial->available()) notifyReadByteListener(serial->read());
   }
 
   void write(uint8_t byte) override {
+    // Overrides the write function from CRemoteXYStream
+    // Writes a single byte to the serial object
+
     serial->write(byte);
   }
 
@@ -38,3 +57,4 @@ class CRemoteXYStream_CDCSerial : public CRemoteXYStream {
 #endif  //USBCON
 
 #endif //RemoteXYStream_CDCSerial_h
+
