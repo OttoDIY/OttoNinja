85a5215f9324607c6b9f78cb4d9d3ff1f9d49425
jaiwiwjwjwisn <168209237+jaiwiwjwjwisn@users.noreply.github.com>
2024-05-01 22:33:56 +0600

    comment

--- src/RemoteXYThread.h
+++ src/RemoteXYThread.h
@@ -1,17 +1,21 @@
+// RemoteXYThread.h: Header file for the CRemoteXYThread class
+// This class is responsible for managing a thread that handles RemoteXY
+// communication and input variables.
+
 // Include necessary header files
 #ifndef RemoteXYThread_h
 #define RemoteXYThread_h
 
 // Include other required header files
-#include "RemoteXYApiData.h"
-#include "RemoteXYComm.h"
-#include "RemoteXYConnection.h"
-#include "RemoteXYWire.h"
+#include "RemoteXYApiData.h"        // For CRemoteXYApiData class
+#include "RemoteXYComm.h"           // For CRemoteXYComm class
+#include "RemoteXYConnection.h"     // For CRemoteXYConnection class
+#include "RemoteXYWire.h"            // For CRemoteXYWire class
 
 // Constants used in the class
-#define REMOTEXY_INIT_CRC 0xffff
-#define REMOTEXY_PACKAGE_START_BYTE 0x55
-#define REMOTEXY_THREAD_TIMEOUT 8000
+#define REMOTEXY_INIT_CRC 0xffff    // Initial CRC value
+#define REMOTEXY_PACKAGE_START_BYTE 0x55  // Start byte for a RemoteXY package
+#define REMOTEXY_THREAD_TIMEOUT 8000  // Timeout for the RemoteXY thread
 
 // Class definition: CRemoteXYThread
 class CRemoteXYThread : public CRemoteXYReceivePackageListener {
@@ -44,20 +48,30 @@ class CRemoteXYThread : public CRemoteXYReceivePackageListener {
 
   public:
     // Constructor
+    // Parameters:
+    //   _data: Pointer to the CRemoteXYData object
     CRemoteXYThread (CRemoteXYData * _data);
 
   public:
     // Method to initialize and start the thread
+    // Parameters:
+    //   _conn: Pointer to the CRemoteXYConnection object
+    //   _wire: Pointer to the CRemoteXYWire object
+    //   _stopByTimeOut: Stop the thread by timeout (1) or not (0)
     void begin (CRemoteXYConnection * _conn, CRemoteXYWire * _wire, uint8_t _stopByTimeOut);
 
     // Method to stop the thread
     void stop ();
 
     // Method to check if the thread is running
+    // Returns:
+    //   1 if the thread is running, 0 otherwise
     uint8_t running ();
 
   public:
     // Overridden method from CRemoteXYReceivePackageListener
+    // Parameters:
+    //   package: Pointer to the received RemoteXY package
     void receivePackage (CRemoteXYPackage * package) override;
 
   private:
@@ -73,9 +87,18 @@ class CRemoteXYThread : public CRemoteXYReceivePackageListener {
 
   public:
     // Static method to start a thread
+    // Parameters:
+    //   data: Pointer to the CRemoteXYData object
+    //   conn: Pointer to the CRemoteXYConnection object
+    //   wire: Pointer to the CRemoteXYWire object
+    //   stopByTimeOut: Stop the thread by timeout (1) or not (0)
     static void startThread (CRemoteXYData * data, CRemoteXYConnection * conn,  CRemoteXYWire * wire, uint8_t stopByTimeOut);
 
     // Static method to get the running thread count
+    // Parameters:
+    //   data: Pointer to the CRemoteXYData object
+    // Returns:
+    //   The number of running threads
     static uint8_t runningCount (CRemoteXYData * data);
 
 };
